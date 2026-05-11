675deae478dd1d239d071ba06e3511a94b867dd8
YangDaJin0104 <didekwls0104@naver.com>
2026-05-11 18:36:03 +0900

    [init] initial commit

---
+++ src/main/java/featurelabs/creev/CreevApplication.java
@@ -0,0 +1,13 @@
+package featurelabs.creev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/featurelabs/creev/CreevApplicationTests.java
@@ -0,0 +1,13 @@
+package featurelabs.creev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
