6fffcbc8d36c6be907e18ffe017c4625f27d635b
YangDaJin0104 <didekwls0104@naver.com>
2026-05-11 19:34:53 +0900

    [build] validation 추가

---
+++ src/main/java/featurelabs/creev/order/api/dto/CreateOrderRequest.java
@@ -0,0 +1,21 @@
+package featurelabs.creev.order.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateOrderRequest(
+
+        @NotNull(message = "사용자 ID는 필수입니다.")
+        Long userId,
+
+        @NotNull(message = "상품 ID는 필수입니다.")
+        Long productId,
+
+        @NotNull(message = "주문 수량은 필수입니다.")
+        @Min(value = 1, message = "주문 수량은 1개 이상이어야 합니다.")
+        Integer quantity,
+
+        @NotBlank(message = "멱등키는 필수입니다.")
+        @Size(max = 100, message = "멱등키는 100자를 초과할 수 없습니다.")
+        String idempotencyKey
+) {
+}
