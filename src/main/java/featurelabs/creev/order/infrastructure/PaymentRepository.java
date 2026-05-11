9ca0efa8dacd97ede97480502000a1865f5d41f3
YangDaJin0104 <didekwls0104@naver.com>
2026-05-11 19:18:23 +0900

    [feat] order repository 추가

---
+++ src/main/java/featurelabs/creev/order/infrastructure/PaymentRepository.java
@@ -0,0 +1,11 @@
+package featurelabs.creev.order.infrastructure;
+
+import featurelabs.creev.order.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Optional<Payment> findByOrderId(Long orderId);
+}
