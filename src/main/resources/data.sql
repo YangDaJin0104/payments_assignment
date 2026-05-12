fca4d1a1bec287c9f2ae677e8fc90b023e1a5427
YangDaJin0104 <didekwls0104@naver.com>
2026-05-12 20:43:30 +0900

    [feat/#15] 피드 커서 페이지 구현

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -23,3 +23,51 @@ insert into product_stocks (
              0,
              0
          );
+insert into feeds (
+    id,
+    product_id,
+    title,
+    video_url,
+    thumbnail_url,
+    created_at
+) values
+      (
+          1,
+          1,
+          '남성 아우터 숏폼 1',
+          'https://example.com/video/1.mp4',
+          'https://example.com/thumb/1.jpg',
+          TIMESTAMP '2026-05-11 10:00:00'
+      ),
+      (
+          2,
+          1,
+          '남성 아우터 숏폼 2',
+          'https://example.com/video/2.mp4',
+          'https://example.com/thumb/2.jpg',
+          TIMESTAMP '2026-05-11 10:01:00'
+      ),
+      (
+          3,
+          1,
+          '남성 아우터 숏폼 3',
+          'https://example.com/video/3.mp4',
+          'https://example.com/thumb/3.jpg',
+          TIMESTAMP '2026-05-11 10:02:00'
+      ),
+      (
+          4,
+          1,
+          '남성 아우터 숏폼 4',
+          'https://example.com/video/4.mp4',
+          'https://example.com/thumb/4.jpg',
+          TIMESTAMP '2026-05-11 10:03:00'
+      ),
+      (
+          5,
+          1,
+          '남성 아우터 숏폼 5',
+          'https://example.com/video/5.mp4',
+          'https://example.com/thumb/5.jpg',
+          TIMESTAMP '2026-05-11 10:04:00'
+      );
